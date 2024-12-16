a918eff2a9343b08feeae007b63ce1e1682e8ee6
migacur <78935454+migacur@users.noreply.github.com>
2024-12-16 11:19:16 -0400

    cambio axios config

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crear una instancia de Axios
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:3000', 
+    baseURL: 'https://migpics-backend.onrender.com', 
 });
 
 
